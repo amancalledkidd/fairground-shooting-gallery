b6d35105878a60369042e7665dcef88bd1ae6b38
Kumani K <kumanikidd@gmail.com>
2023-12-04 14:33:12 +0000

    added background and simple click feature

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 import './style.css'
+
+const gameMap = document.getElementById('game__area') as HTMLDivElement
+
+if (!gameMap) {
+  throw new Error('Game map not found')
+}
+
+const showTarget = () => {
+  gameMap.innerHTML = `<div class="target">🎯</div>`
+}
+
+showTarget()
+
+
+const handleShot = (event: MouseEvent) => {
+    const target = event.target as HTMLDivElement
+    if (target.classList.contains('target')) {
+        target.remove()
+        console.log("hello")
+        
+    }
+}
+
+
+gameMap.addEventListener('click', handleShot)
