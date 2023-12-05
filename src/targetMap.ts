e1f8391429ebe338330995e0973d21bcc351c91f
Kumani K <kumanikidd@gmail.com>
2023-12-05 21:24:12 +0000

    added fixed postions for targets

--- src/targetMap.ts
+++ src/targetMap.ts
@@ -3,9 +3,34 @@ interface TargetLocation {
     y: number
 }
 
-const rows: number = 10
-const cols: number = 10
-const targetSize: number = 20
+export const fixedTargetArray: TargetLocation[] = [
+    { x: 49.5, y: 69 },
+    { x: 65.5, y: 69 },
+    { x: 78.5, y: 69 },
+    { x: 35.3, y: 69 },
+    { x: 21.3, y: 69 },
+    { x: 10.3, y: 68.9 },
+    { x: 10.5, y: 50.9 },
+    { x: 10.9, y: 37.8 },
+    { x: 21, y: 37.8 },
+    { x: 29.5, y: 38.8 },
+    { x: 36.5, y: 39.8 },
+    { x: 38.5, y: 30.8 },
+    { x: 18.5, y: 25.8 },
+    { x: 20.5, y: 51 },
+    { x: 24.5, y: 51 },
+    { x: 36.5, y: 50.5 },
+    { x: 42, y: 50.5 },
+    { x: 85.5, y: 50.5 },
+    { x: 55.5, y: 45.5 },
+    { x: 83.5, y: 32.5 },
+    { x: 77.5, y: 41.5 },
+    { x: 64.5, y: 79.5 }
+]
+
+const rows: number = 5
+const cols: number = 0
+const targetSize: number = 10
 
 export const targetMap= (): TargetLocation[] => {
     const targetArray: TargetLocation[] = [] 
