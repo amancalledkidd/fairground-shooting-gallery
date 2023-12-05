b99cc3b043f502a5987a9040daa95bf457fdf486
Kumani K <kumanikidd@gmail.com>
2023-12-05 19:07:44 +0000

    updated target mapping function

--- src/targetMap.ts
+++ src/targetMap.ts
@@ -1,8 +1,22 @@
-export const targetMap = () => {
-    const targetArray: number[] = []  
-    for (let i = 0; i < 11; i++) {
-        for (let j = 0; j < 25; j++) {
-            targetArray.push(0)
+interface TargetLocation {
+    x: number
+    y: number
+}
+
+const rows: number = 10
+const cols: number = 10
+const targetSize: number = 20
+
+export const targetMap= (): TargetLocation[] => {
+    const targetArray: TargetLocation[] = [] 
+    const spacingX = (650 - cols * targetSize) / (cols + 1);
+    const spacingY = (650 - rows * targetSize) / (rows + 1);
+    
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const x = j * (targetSize + spacingX) + spacingX;
+            const y = i * (targetSize + spacingY) + spacingY;
+            targetArray.push({ x, y });
         }
     }
     return targetArray
