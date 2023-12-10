b091a81298fd6e1369634a2766a270790180542f
Kumani K <kumanikidd@gmail.com>
2023-12-10 09:59:06 +0000

    cleaned up code, moved target and sound functions to new files and imported them

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,103 @@
 import './style.css'
-import { targetMap, fixedTargetArray } from './targetMap'
+import { randomScatterTarget, movingTarget } from './targetMap'
+import { playMetalTargetHit, playGunShot, playGlassTargetHit } from './soundEffects'
 
 // Get HTML elements
 const gameMap = document.getElementById('game__area') as HTMLDivElement
-const gridContainer = document.querySelector('.grid-container') as HTMLDivElement
 const pointsCount = document.querySelector('#score__card--points') as HTMLDivElement
 const multiplierCard = document.querySelector('#score__card--multiplier') as HTMLDivElement
 const timerCard = document.querySelector('#score__card--timer') as HTMLDivElement
-const gunShot = document.querySelector('#gunshot') as HTMLAudioElement
+const startButton = document.querySelector('#start__button') as HTMLButtonElement
+
 
 // Error handling
-if (!gameMap || !gridContainer) {
+if (!gameMap) {
     throw new Error('Game map not found')
+}
+
+if (!startButton) {
+    throw new Error('Start button not found')
 }
 
 if (!pointsCount || !multiplierCard || !timerCard) {
     throw new Error('Score card not found')
 }
-if (!gunShot) {
-    throw new Error('Gunshot sound not found')
-}
+
 
 let score: number = 0
 let shots: number = 0
 let hits: number = 0
 let multiplier: number = 1
 
-const startGame = () => {
-    gameTimer()
-    setInterval(fixedRandomTarget, 1200)
-}
 
-
-const getMultiplier = () => {
+// TODO: make more efficent
+const getMultiplier = (multiplier: number) => {
     let htmlText = `Multiplier: x${multiplier}`
     if (hits > 10) {
         multiplier = 5
     } else if  (hits > 3) {
         multiplier = 2
     } else {
-        multiplier =1
+        multiplier = 1
     }
     multiplierCard.innerText = htmlText
 }
 
+const handleStartClick = () => {
+    startButton.innerText = 'Restart'
+    startGame()
+}
 
+const startGame = () => {
+    let score: number = 0
+    let shots: number = 0
+    let hits: number = 0
+    let multiplier: number = 1
+
+    movingTarget("target", 5, "H", 10, 30);
+    movingTarget("bottle", 15, "H", 7, 93.5, 0.7, true);
+    // movingTarget("target", 5, "H", 10, 55);
+    // movingTarget("target", 5, "H", 10, 70);
+    movingTarget("target", 5, "V", 5, 20);
+    randomScatterTarget("ufo", 90, 90, true);
+
+}
 // handleShot function
 const handleShot = (event: MouseEvent) => {
+    // get target element that was clicked
     const target = event.target as HTMLDivElement
     shots += 1
-    playSoundShot()
+    // play a gunshot sound
+    playGunShot()
+    // if target is a target, remove it depnding on type
     if (target.classList.contains('target')) {
-        playSoundHit()
+       targetHit()
+       target.remove()
+    } else if (target.classList.contains('bottle')) {
+        bottleHit()
         target.remove()
-        getMultiplier()
-        score += 10 * multiplier
-        hits += 1
-        updateScore()
-    } // Include sound effect here
+    }
     else {
         hits = 0
     }
 }
 
-const playSoundShot = () => {
-    gunShot.volume = 0.1
-    gunShot.currentTime = 0
-    gunShot.play()
+const targetHit = () => {
+    playMetalTargetHit()
+    score += 10 * multiplier
+    hits += 1
+    updateScore()
 }
 
-const playSoundHit = () => {
-    const hit = document.querySelector('#hit') as HTMLAudioElement
-    hit.volume = 0.2
-    hit.currentTime = 0
-    hit.play()
+
+const bottleHit = () => {
+    playGlassTargetHit()
+    score += 20 * multiplier
+    hits += 1
+    updateScore()
 }
+
+
 
 const updateScore = () => {
     pointsCount.textContent = `Score: ${score .toString()}`
@@ -94,42 +117,7 @@ const gameTimer = () => {
     }, 1000)
 }
 
-// Layout grid for targets
-// Should be part of startGame function
-const targetArray = targetMap()
 
-// Create grid for HTML to allow for targets
-// Should be part of startGame function
-const fixedRandomTarget = (): void => {
-    const randomIndex = Math.floor(Math.random() * fixedTargetArray.length);
-    const position = fixedTargetArray[randomIndex];
-
-    const targetElement = document.createElement('div');
-    targetElement.classList.add('target');
-    targetElement.style.left = `${position.x}%`;
-    targetElement.style.top = `${position.y}%`;
-    gameMap.appendChild(targetElement);
-}
-
-const ufoMovingTarget = (): void => {
-    setInterval(() => {
-        const alien = document.querySelector('.alien') as HTMLDivElement
-        if (alien) {
-            alien.remove()
-        }
-        
-        const targetElement = document.createElement('div');
-        targetElement.classList.add('alien');
-        targetElement.style.left = `${Math.floor(Math.random() * 100)}%`;
-        targetElement.style.top = `15%`;
-        gameMap.appendChild(targetElement);
-}, 700)
-}
-
-// interval for randomTargets to appear
-// setInterval(fixedRandomTarget, 1400)
-startGame()
-// setInterval(fixedRandomTarget, 1200)
-// ufoMovingTarget()
-// eevent listener for shots
 gameMap.addEventListener('click', handleShot)
+startButton.addEventListener('click', handleStartClick)
+
